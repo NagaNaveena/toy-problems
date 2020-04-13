3b62640b0011279ef76cec17c456a1cd323e8c98
Naveena <naveena.madamanchi@msitprogram.net>
2020-04-14 00:19:43 +0530

    written public methods(API)  for LRU without logical code

--- LRU_cache.py
+++ LRU_cache.py
@@ -3,4 +3,12 @@ class LRU_cache:
     def __init__(self,size):
         self.size = size
     
-    
+    def put(self,):
+        pass
+
+    def get(self,):
+        pass
+
+    def get_cache(self,):
+        pass
+
