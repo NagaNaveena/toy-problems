de6dd8ba658f9b3162f82d9cabd600e50e753309
Naveena <naveena.madamanchi@msitprogram.net>
2020-04-14 22:00:11 +0530

    Implemented all functions. all test cases are passed.

--- LRU_test.py
+++ LRU_test.py
@@ -5,11 +5,16 @@ class LRU_test:
 
     def main():
         lru_obj = LRU_cache(4)
-        assert 1 == lru_obj.put(), "put failed"
+        lru_obj.put(1)
+        lru_obj.put(2)
+        lru_obj.put(3)
+        lru_obj.put(4)
+        lru_obj.put(5)
 
-        assert 2 == lru_obj.get(1) ,"get failed"
-
-        assert [1,2] == lru_obj.get_cache(), "get_cache failed"
+        assert 3 == lru_obj.get(1) ,"get failed"
+        print(lru_obj.get_cache())
+        assert [2,4,5,3] == lru_obj.get_cache(), "get_cache failed"
+        print("all passed")
        
 if __name__ == '__main__':
     LRU_test.main()
